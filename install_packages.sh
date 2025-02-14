3b260ca5687e0c6ca24a044b6937e37cb5e90576
Sal <altaf.saljooq@outlook.com>
2025-02-13 21:06:13 -0600

    added script to add the needed package for the webserver

---
+++ install_packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# List of packages to check
+PACKAGES=("nodejs" "npm" "golang-go" "nginx")
+
+# Update package list
+sudo apt update
+
+# Loop through each package in the list
+for PACKAGE_NAME in "${PACKAGES[@]}"; do
+  # Check if the package is installed
+  dpkg -l | grep -qw "$PACKAGE_NAME"
+
+  # If the package is not installed, install it
+  if [ $? -ne 0 ]; then
+    echo "$PACKAGE_NAME is not installed. Installing..."
+    sudo apt install -y "$PACKAGE_NAME"
+  else
+    echo "$PACKAGE_NAME is already installed."
+  fi
+done
+
+systemctl start nginx
+systemctl enable nginx
